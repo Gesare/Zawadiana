87a660ac811a1ebe8fd383174d1b79c6a6d335c4
Gesare <gesareayienda@gmail.com>
2020-06-07 21:38:39 +0300

    Putting rating and js

--- awwardapp/views.py
+++ awwardapp/views.py
@@ -3,7 +3,7 @@ from django.templatetags.static import static
 from django.shortcuts import render, redirect, render_to_response, HttpResponseRedirect
 from django.http import HttpResponse, Http404
 import datetime as dt
-from .models import *
+from .models import Profile,Projects
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
 from .forms import *
