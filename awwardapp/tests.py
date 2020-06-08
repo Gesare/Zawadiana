414a432b82cc384bed0b8ee28b8fd8b0d32572e6
Gesare <gesareayienda@gmail.com>
2020-06-08 18:30:44 +0300

    DRY

--- awwardapp/tests.py
+++ awwardapp/tests.py
@@ -1,26 +1,47 @@
 from django.test import TestCase
-from .models import Profile,Projects
-from django.contrib.auth.models import User
+from .models import Profile,Project
+from django.core.files.uploadedfile import SimpleUploadedFile
+import datetime as dt
 
-class TestProfile(TestCase):
-    def setUp(self):
-        self.user = User(username='Gesare')
-        self.user.save()
+# Create your tests here.
 
-        self.profile_test = Profile(id=1, name='image', profile_picture='image.jpg', bio='We are testing the profile ', user=self.user)
+class ProfileTest(TestCase):
+  def setUp(self):
+        self.user_ges = User.objects.create_user(username = 'Gesare', password = 'gesare2000')
+    
+  def test_update_bio(self):
+    self.user_ges.profile.update_profile_bio("God is good")
+    self.assertEqual(self.user_kim.profile.profile_bio,"God is good")
 
-    def test_instance(self):
-        self.assertTrue(isinstance(self.profile_test, Profile))
+  def test_search_user_profile(self):
+    results =Profile.search_user_profile("Gesare")
+    self.assertTrue(len(results)>0)
 
-    def test_save_profile(self):
-        self.profile_test.save_profile()
-        after = Profile.objects.all()
-        self.assertTrue(len(after) > 0)
+  def test_get_user_profile(self):
+    userprof = Profile.get_user_profile(self.user_ges)
+    self.assertEqual(userprof.user.username,self.user_ges.username)
 
-class ProjectsTestCase(TestCase):
-    def setUp(self, project_name='GitSearch', project_photo='lol.jpg', description='some description', github_repo='git repo', url='lol.com', owner='Mercurial'):
-        return Projects.objects.create(project_name=project_name, project_photo=project_photo, description=description, github_repo=github_repo, url=url, owner=owner)
+class ProjectTest(TestCase):
+  def setUp(self):
+    
+    self.new_project = Project(title = "KKK",description = "We are a family",live_site = "amazing,org",profile = self.user_kim.profile, pub_date=dt.date.today())
+    self.new_project.landing_page = SimpleUploadedFile(name='west.jpg',content=open('/home/martin/Documents/Moringa-Core/Django/awward/media/west.jpg','rb').read(),content_type='image/jpeg')
 
-    def projectSave(self):
-        initialization = self.setUp()
-        self.assertTrue(save > 0)
+    self.new_project.save()
+
+  def test_get_single_project(self):
+    project = Project.get_single_project(self.new_project.id)
+    self.assertEqual(self.new_project.title,"KKK")
+
+  def test_get_all_user_projects(self):
+    projects = Project.get_all_projects_user(self.user_kim.profile)
+    self.assertTrue(len(projects)==1)
+
+  def test_get_all_projects(self):
+    projects = Project.get_all_projects()
+    self.assertTrue(len(projects)==1)
+
+  def test_search_project_title(self):
+    searched_projects = Project.search_project_title("KKK")
+    self.assertTrue(len(searched_projects)==1)
+
